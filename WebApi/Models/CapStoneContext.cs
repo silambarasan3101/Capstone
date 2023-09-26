31471cecd47e9ec8e2cde4fa346c44543f9f4a47
Silambarasan <msilambarasan98@gmail.com>
2023-09-26 15:15:49 +0530

    first commit

---
+++ WebApi/Models/CapStoneContext.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models;
+
+public partial class CapStoneContext : DbContext
+{
+    public CapStoneContext()
+    {
+    }
+
+    public CapStoneContext(DbContextOptions<CapStoneContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<AdminInfo> AdminInfos { get; set; }
+
+    public virtual DbSet<BlogInfo> BlogInfos { get; set; }
+
+    public virtual DbSet<EmpInfo> EmpInfos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=DESKTOP-R52GF1S;Database=CapStone;Trusted_Connection=True;TrustServerCertificate=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AdminInfo>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__AdminInf__3214EC07B8270755");
+
+            entity.ToTable("AdminInfo");
+
+            entity.Property(e => e.EmailId)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<BlogInfo>(entity =>
+        {
+            entity.HasKey(e => e.BlogId).HasName("PK__BlogInfo__54379E302BF43C34");
+
+            entity.ToTable("BlogInfo");
+
+            entity.Property(e => e.BlogUrl)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.DateOfCreation).HasColumnType("datetime");
+            entity.Property(e => e.EmpEmailId)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Subject)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Title)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<EmpInfo>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__EmpInfo__3214EC07E9D8D724");
+
+            entity.ToTable("EmpInfo");
+
+            entity.HasIndex(e => e.EmailId, "UQ__EmpInfo__7ED91ACECBE35D19").IsUnique();
+
+            entity.Property(e => e.DateOfJoining).HasColumnType("datetime");
+            entity.Property(e => e.EmailId)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
